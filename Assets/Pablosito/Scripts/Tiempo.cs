f19cd7521369b83a985c7e2197824d3bb302826e
pablosito <pablogonzaloarruti@gmail.com>
2022-04-05 00:09:00 +0200

    .

--- Assets/Pablosito/Scripts/Tiempo.cs
+++ Assets/Pablosito/Scripts/Tiempo.cs
@@ -2,18 +2,23 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
 public class Tiempo : MonoBehaviour
 {
     public static float tiempo;
-    public float tiempoInicial;
+    public static float tiempoInicialA;
+    public Text aTiempo;
 
     private void Awake()
     {
-        tiempo = tiempoInicial;
+        tiempoInicialA = 100;
+        tiempo = tiempoInicialA;
     }
 
     void Update()
     {
+        aTiempo.text = tiempo.ToString();
         tiempo -= Time.deltaTime;
         if(tiempo<=0)
         {
