0726a52e22843962876925a9715c3830531fc31a
pablosito <pablogonzaloarruti@gmail.com>
2022-04-29 20:22:54 +0200

    Pablosito

--- Assets/Pablosito/Scripts/Tiempo.cs
+++ Assets/Pablosito/Scripts/Tiempo.cs
@@ -9,25 +9,42 @@ public class Tiempo : MonoBehaviour
     public static float tiempo;
     public static float tiempoInicialA;
     public Text aTiempo;
+    public float tiempoI;
 
     private void Awake()
     {
-        tiempoInicialA = 100;
+        tiempoInicialA = 2;
         tiempo = tiempoInicialA;
     }
 
     void Update()
     {
-        aTiempo.text = tiempo.ToString();
+        tiempoI = tiempo;
         tiempo -= Time.deltaTime;
         if(tiempo<=0)
         {
+            Debug.Log("si");
             Muerte();
         }
+
+        DisplayTime(tiempo);
     }
 
+    void DisplayTime(float timeToDisplay)
+    {
+        if(timeToDisplay <0)
+        {
+            timeToDisplay = 0;
+        }
+        float minutes = Mathf.FloorToInt(timeToDisplay / 60);
+        float seconds = Mathf.FloorToInt(timeToDisplay % 60);
+
+        aTiempo.text = string.Format("{0:00}:{1:00}", minutes, seconds);
+        //aTiempo.text = tiempo.ToString();
+    }
     void Muerte()
     {
-        SceneManager.LoadScene("");
+        Debug.Log("no");
+        SceneManager.LoadScene("GameOver");
     }
 }
