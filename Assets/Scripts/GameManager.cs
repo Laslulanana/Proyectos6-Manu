4c4ccb6dd15a462cdaa0d75cdbc1c483c3ddb495
pablosito <pablogonzaloarruti@gmail.com>
2022-05-30 23:56:23 +0200

    .

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -20,7 +20,7 @@ public class GameManager : MonoBehaviour
     void Start()
     {
         instance = this;
-        DontDestroyOnLoad(gameObject);
+        //DontDestroyOnLoad(gameObject);
         SortItems();
     }
 
